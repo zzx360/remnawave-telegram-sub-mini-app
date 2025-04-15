0f41966ad98cee352c7c50be30b9cdf275a532ff
Vitalii Miagkov <maposia@gmail.com>
2025-04-15 09:29:47 +0300

    change func get user

--- src/api/fetchUserByTgId.ts
+++ src/api/fetchUserByTgId.ts
@@ -1,24 +1,20 @@
-import {subscriptionsResponse} from "@/types/types";
+import { subscriptionsResponse } from "@/types/types";
 
 export async function fetchUserByTelegramId(telegramId: number) {
-
     try {
-        const res = await fetch('/api/getSubscriptionInfo', {
-            method: 'POST',
+        const res = await fetch(`/api/getSubscriptionInfo?telegramId=${telegramId}`, {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ telegramId }),
         });
 
         if (!res.ok) {
-            throw new Error('Ошибка при запросе данных.');
+            throw new Error('Error while fetching data from the remote API.');
         }
 
-        const { response } : subscriptionsResponse = await res.json();
-        return response[0]
-
-
+        const { response }: subscriptionsResponse = await res.json();
+        return response[0];
     } catch (error) {
         console.error('Ошибка:', error);
     }
--- src/app/api/getSubscriptionInfo/route.ts
+++ src/app/api/getSubscriptionInfo/route.ts
@@ -1,22 +1,28 @@
-export async function POST(request: Request) {
+export async function GET(request: Request) {
     try {
-        const body = await request.json();
-        const { telegramId } = body;
+        const { searchParams } = new URL(request.url);
+        const telegramId = searchParams.get('telegramId');
 
+        if (!telegramId) {
+            return new Response(
+                JSON.stringify({ error: 'telegramId is required' }),
+                { status: 400 }
+            );
+        }
 
-        const baseUrl = process.env.REMNAWAVE_URL
-        const token = process.env.REMNAWAVE_TOKEN
-        const httpMode  = process.env.REMNAWAVE_MODE === 'local' ? 'http' : 'https'
-        const url = `${httpMode}://${baseUrl}/api/users/tg/${telegramId}`
+        const baseUrl = process.env.REMNAWAVE_URL;
+        const token = process.env.REMNAWAVE_TOKEN;
+        const httpMode = process.env.REMNAWAVE_MODE === 'local' ? 'http' : 'https';
+        const url = `${httpMode}://${baseUrl}/api/users/tg/${telegramId}`;
         const localHeadersParam = {
             'x-forwarded-for': '127.0.0.1',
-            'x-forwarded-proto': 'https'
-        }
+            'x-forwarded-proto': 'https',
+        };
 
         const headers = {
             Authorization: `Bearer ${token}`,
             ...(httpMode === 'http' ? localHeadersParam : {}),
-        }
+        };
 
         const res = await fetch(url, {
             method: 'GET',
@@ -33,7 +39,6 @@ export async function POST(request: Request) {
 
         const data = await res.json();
         return new Response(JSON.stringify(data), { status: 200 });
-
     } catch (error) {
         console.error('Error:', error);
         return new Response(JSON.stringify({ error: 'Server error.' }), { status: 500 });
