02b444351db2aba70c25b9bdf51d8879ac9b1424
Vitalii Miagkov <maposia@gmail.com>
2025-05-09 14:10:40 +0300

    feat: update ErrorBoundary UI

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import Image from "next/image";
+import {Center, Flex, Title} from "@mantine/core";
 
 export function ErrorPage({
   error,
@@ -14,13 +16,18 @@ export function ErrorPage({
 
   return (
     <div>
-      <h2>An unhandled error occurred!</h2>
-      <blockquote>
-        <code>
-          {error.message}
-        </code>
-      </blockquote>
+        <Center style={{ height: '100vh'}}>
+            <Flex justify='center' align='center' direction='column' gap='xl'>
+        <Title ta='center' order={1} size={20}>The app must be opened through Telegram</Title>
+            <Image
+                alt="Telegram sticker"
+                src="/assets/telegram.gif"
+                width={144}
+                height={144}
+            />
       {reset && <button onClick={() => reset()}>Try again</button>}
+            </Flex>
+        </Center>
     </div>
   );
 }
